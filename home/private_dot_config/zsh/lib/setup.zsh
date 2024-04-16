82b16cc990bd51e3cd77f633dcdff44ebfaa17f7
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-04-16 23:25:29 +0800

    Add Miniforge and Rust

--- home/private_dot_config/zsh/lib/setup.zsh
+++ home/private_dot_config/zsh/lib/setup.zsh
@@ -4,10 +4,6 @@
 
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/philip/package/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/philip/package/fzf/bin"
-fi
-
 if command -v fzf > /dev/null 2>&1; then
   eval "$(fzf --zsh)"
 fi
@@ -16,4 +12,22 @@ fi
 # ------------
 if command -v zoxide > /dev/null 2>&1; then
   eval "$(zoxide init --cmd cd zsh)"
+fi
+
+# Setup miniforge
+# ---------------
+__conda_setup="$('/home/philip/package/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/philip/package/miniforge3/etc/profile.d/conda.sh" ]; then
+        . "/home/philip/package/miniforge3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/philip/package/miniforge3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+if [ -f "/home/philip/package/miniforge3/etc/profile.d/mamba.sh" ]; then
+    . "/home/philip/package/miniforge3/etc/profile.d/mamba.sh"
 fi
