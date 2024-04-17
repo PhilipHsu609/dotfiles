0623ebcddf8d1dbb305785ecaf3cbaf599362f27
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-04-18 00:15:22 +0800

    Add eza and change fzf preview color

--- home/private_dot_config/zsh/lib/alias.zsh
+++ home/private_dot_config/zsh/lib/alias.zsh
@@ -1,10 +1,14 @@
 # history will ingore commands that start with a space
 alias pwd=' pwd'
 alias clear=' clear'
+
 alias ls=' ls --color=auto'
 alias ll=' ls -alFh'
 alias la=' ls -A'
 alias l=' ls -F'
+
+alias e=' eza --all'
+alias el=' eza --long --all --group-directories-first --time-style=long-iso'
 
 alias cz='chezmoi'
 alias mbd='mamba deactivate'
--- home/private_dot_config/zsh/lib/exports.zsh
+++ home/private_dot_config/zsh/lib/exports.zsh
@@ -9,7 +9,8 @@ export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}
 export PULSE_SERVER=tcp:$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}') # GWSL
 
 # fzf
-FZF_COLOR='bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
+
+FZF_COLOR='bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8'
 FZF_PREVIEW='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {}) 2> /dev/null | head -300'
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --strip-cwd-prefix'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
