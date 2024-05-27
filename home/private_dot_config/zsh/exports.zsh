d9bf129ed6cf64d939595a31406a5e0acb243e78
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-05-27 22:49:58 +0800

    Install packages via brew instead of manually

--- home/private_dot_config/zsh/exports.zsh
+++ home/private_dot_config/zsh/exports.zsh
@@ -8,6 +8,8 @@ export EDITOR='vim'
 export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0      # GWSL
 export PULSE_SERVER=tcp:$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}') # GWSL
 
+export PATH="$HOME/.local/bin:$PATH"
+
 # fzf
 FZF_COLOR='bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8'
 FZF_PREVIEW='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {}) 2> /dev/null | head -300'
@@ -17,7 +19,7 @@ export FZF_ALT_C_COMMAND="fd --type d --hidden --exclude .git --strip-cwd-prefix
 export FZF_DEFAULT_OPTS="--no-mouse --height 50% --layout=reverse --border --ansi --color='$FZF_COLOR' --preview='$FZF_PREVIEW' --bind='ctrl-/:change-preview-window(right|hidden)'"
 
 # bat
-export BAT_THEME='OneHalfDark'
+export BAT_THEME='Catppuccin Macchiato'
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 # ripgrep
