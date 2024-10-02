3dabe64ae6ed54b26474901ec1e2839a04941cd5
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-10-02 09:56:07 +0800

    Replace ls with eza

--- home/private_dot_config/zsh/alias.zsh
+++ home/private_dot_config/zsh/alias.zsh
@@ -2,13 +2,8 @@
 alias pwd=' pwd'
 alias clear=' clear'
 
-alias ls=' ls --color=auto'
-alias ll=' ls -alFh'
-alias la=' ls -A'
-alias l=' ls -F'
-
-alias e=' eza'
-alias el=' eza --long --all --header --group-directories-first --time-style=long-iso'
+alias l=' eza'
+alias ls=' eza --long --all --header --group-directories-first --time-style=long-iso'
 
 alias cz='chezmoi'
 alias mbd='mamba deactivate'
